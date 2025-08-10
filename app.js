2b7e81337ec6a1537e52f006a7d205b81971d8e3
Danilo <daniloalvarezrodriguez@gmail.com>
2025-08-10 10:17:11 -0500

    corregi ortografia

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-alert("Hola munod");// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+alert("Hola mundo");// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
