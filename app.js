cc405862324dc99a3dcedf76577f7781cce672b2
Danilo <daniloalvarezrodriguez@gmail.com>
2025-08-10 11:41:03 -0500

    agregar variable

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-alert("Hola mundo");// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Arreglo para almacenar los nombres de los amigos
+let listaAmigos = [];
+
